3b790fcae2d2ce01da22a7d3e3014419e4f1c280
Brian Dennis <briandennis321@gmail.com>
2016-07-02 16:23:15 -0500

    add more structure

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const app     = require('express')();
+const express = require('express');
+const app     = express();
 const swig    = require('swig');
 const port    = process.env.PORT || 5000;
 
 app.engine('html', swig.renderFile);
 app.set('views', './public/templates');
 app.set('view engine', 'html');
+
+app.use(express.static('public/static'));
 
 app.use( require('./app/router')(app) );
 
